e091acb09f2467f2dd8b8f85bf97d1407c203e46
Luis Berenguer Herrero <luisberenguer96@gmail.com>
2016-12-13 12:01:09 +0100

    Cosas de juanki
    alsdkfja

--- Fight4Fit/Fight4FitGen/Fight4Fit-FrontEnd/Controllers/FotoController.cs
+++ Fight4Fit/Fight4FitGen/Fight4Fit-FrontEnd/Controllers/FotoController.cs
@@ -1,9 +1,15 @@
-﻿using MvcApplication1.Controllers;
+﻿using Fight4Fit_FrontEnd.Models;
+using MvcApplication1.Controllers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Fight4FitGenNHibernate.CEN.Fight4Fit;
+using Fight4FitGenNHibernate.EN.Fight4Fit;
+using Fight4Fit_FrontEnd.Models;
+using System.IO;
+using Fight4FitGenNHibernate.CAD.Fight4Fit;
 
 namespace Fight4Fit_FrontEnd.Controllers
 {
@@ -14,7 +20,10 @@ namespace Fight4Fit_FrontEnd.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            FotoCEN fotCEN = new FotoCEN();
+            IEnumerable<FotoEN> list = fotCEN.ReadAll(0, -1).ToList();
+            
+            return View(list);
         }
 
         //
@@ -22,26 +31,50 @@ namespace Fight4Fit_FrontEnd.Controllers
 
         public ActionResult Details(int id)
         {
-            return View();
+            FotoModelo fotomod = null;
+            SessionInitialize();
+            //FotoCAD fotCAD = new FotoCAD(session);
+            FotoEN fotEN = new FotoCAD(session).ReadOIDDefault(id);
+            fotomod = new FotoAssembler().ConvertENToModelUI(fotEN);
+            SessionClose();
+            return View(fotomod);
+           
         }
 
         //
         // GET: /Foto/Create
 
-        public ActionResult Create()
+        public ActionResult Create(String nom)
         {
-            return View();
+            FotoModelo fotomod = new FotoModelo();
+            fotomod.nombre = nom;
+            return View(fotomod);
         }
 
         //
         // POST: /Foto/Create
 
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(FotoModelo fotomod, HttpPostedFileBase file)
         {
+         String fileName = "", pathh = "";
+            
+            if(file!= null && file.ContentLength>0)
+            {
+                fileName = Path.GetFileName(file.FileName);
+                pathh = Path.Combine(Server.MapPath("~/Images/Uploads"), fileName);
+                file.SaveAs(pathh);
+            }
+
+
             try
             {
                 // TODO: Add insert logic here
+                fileName = "/Images/Uploads" + fileName;
+                FotoCEN cen = new FotoCEN();
+                //string p_Nombre, string p_Usuario, Nullable<DateTime> p_Fecha, string p_Descripcion, string p_Imagen, int p_pertenece_a, int p_likes)
+                cen.SubirFoto(fotomod.nombre, fotomod.usuario, fotomod.fecha, fotomod.descripcion, fotomod.imagen, fotomod.pertenece, fotomod.likes); 
+                //no se sista bien
 
                 return RedirectToAction("Index");
             }
@@ -50,13 +83,19 @@ namespace Fight4Fit_FrontEnd.Controllers
                 return View();
             }
         }
+        }
 
         //
         // GET: /Foto/Edit/5
 
         public ActionResult Edit(int id)
         {
-            return View();
+            FotoModelo fotomod = null;
+            SessionInitialize();
+            FotoEN fotEN = new FotoCAD(session).ReadOIDDefault(id);
+            fotomod = new FotoAssembler().ConvertENToModelUI(fotEN);
+            SessionClose();
+            return View(fotomod);
         }
 
         //
@@ -68,6 +107,8 @@ namespace Fight4Fit_FrontEnd.Controllers
             try
             {
                 // TODO: Add update logic here
+                FotoCEN fotCEN = new FotoCEN();
+                fotCEN.Modify(id);
 
                 return RedirectToAction("Index");
             }
@@ -82,7 +123,29 @@ namespace Fight4Fit_FrontEnd.Controllers
 
         public ActionResult Delete(int id)
         {
-            return View();
+            
+            try
+            {
+                // TODO: Add delete logic here
+                int foto = -1;
+                SessionInitialize();
+                FotoCAD fotCAD = new FotoCAD(session);
+                FotoCEN fotCEN = new FotoCEN(fotCAD);
+                FotoEN fotEN = fotCEN.ReadOID(id);
+                FotoModelo fotomod = new AssemblerFoto().ConvertENToModelUI(fotEN);
+               // idCategoria = fotomod.IdCategoria; cambiar este
+                SessionClose();
+
+                new FotoCEN().Destroy(id);
+                
+
+               //  return RedirectToAction("PorCategoria", new {id=idCategoria}); ESTO CMABIARÑLO
+            }
+            catch
+            {
+                return View();
+            }            
+
         }
 
         //
---
+++ Fight4Fit/Fight4FitGen/Fight4Fit-FrontEnd/Models/FotoModelo.cs
@@ -0,0 +1,48 @@
+﻿using Fight4FitGenNHibernate.EN.Fight4Fit;
+using MvcApplication1.Controllers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fight4Fit_FrontEnd.Models
+{
+    public class Foto
+    {
+
+        //
+        // GET: /FotoModelo/
+        [ScaffoldColumn(false)]
+        public String id { get; set; }
+
+        [ScaffoldColumn(false)]
+        public String comentario { get; set; }
+
+        [ScaffoldColumn(false)]
+        public String reporte { get; set; }
+
+        [ScaffoldColumn(false)]
+        public String nombre { get; set; }
+
+        [ScaffoldColumn(false)]
+        public FotoEN usuario { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime fecha { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string descripcion { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string imagen { get; set; }
+
+        [ScaffoldColumn(false)]
+        public GaleriaEN usuario { get; set; }
+
+        [ScaffoldColumn(false)]
+        public int likes { get; set; }
+
+    }
+}
