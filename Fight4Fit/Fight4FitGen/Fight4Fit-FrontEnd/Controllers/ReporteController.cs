fdccb498f953085a96ed1f0b2bd55f73ec1940bf
Luis Berenguer Herrero <luisberenguer96@gmail.com>
2016-12-29 20:29:32 +0100

    Avanzando en Reporte

---
+++ Fight4Fit/Fight4FitGen/Fight4Fit-FrontEnd/Controllers/ReporteController.cs
@@ -0,0 +1,111 @@
+﻿using Fight4Fit_FrontEnd.Models;
+using Fight4FitGenNHibernate.CAD.Fight4Fit;
+using Fight4FitGenNHibernate.CEN.Fight4Fit;
+using Fight4FitGenNHibernate.CP.Fight4Fit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fight4Fit_FrontEnd.Controllers
+{
+    public class ReporteController : Controller
+    {
+        //
+        // GET: /Reporte/
+
+        public ActionResult Index()
+        {
+            ReporteModelo rep = new ReporteModelo();
+            return View(rep);
+        }
+
+        //
+        // GET: /Reporte/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Reporte/Create
+
+        public ActionResult Create()
+        {
+            ReporteModelo rep = new ReporteModelo();
+            return View(rep);
+        }
+
+        //
+        // POST: /Reporte/Create
+
+        [HttpPost]
+        public ActionResult Create(ReporteModelo repMod, HttpPostedFileBase file)
+        {
+            try
+            {
+                ReporteCP repCP = new ReporteCP();
+                repCP.NuevoReporte(repMod.texto, repMod.motivo, repMod.email, repMod.tipo, repMod.id);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Reporte/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Reporte/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Reporte/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Reporte/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Fight4Fit/Fight4FitGen/Fight4Fit-FrontEnd/Models/ReporteModelo.cs
@@ -0,0 +1,27 @@
+﻿using Fight4FitGenNHibernate.Enumerated.Fight4Fit;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Fight4Fit_FrontEnd.Models
+{
+    public class ReporteModelo
+    {
+        [ScaffoldColumn(false)]
+        public String email { get; set; }
+
+        [ScaffoldColumn(false)]
+        public int id { get; set; }
+
+        [ScaffoldColumn(false)]
+        public String texto { get; set; }
+
+        [ScaffoldColumn(false)]
+        public MotivoEnum motivo { get; set; }
+
+        [ScaffoldColumn(false)]
+        public TipoReporteEnum tipo { get; set; }
+    }
+}
